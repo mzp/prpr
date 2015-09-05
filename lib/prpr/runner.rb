274aac46b8d9f9720866fb7cc7650cac27e9db62
mzp <mzpppp@gmail.com>
2015-09-05 14:26:14 +0900

    :gem: require all gems before running

--- lib/prpr/runner.rb
+++ lib/prpr/runner.rb
@@ -1,8 +1,17 @@
 module Prpr
   class Runner
+    def initialize
+      bundle
+    end
+
     def call(payload, event:)
       e = Prpr::Event::Event.parse(payload, event: event)
       Prpr::Handler::Base.on_event e
+    end
+
+    def bundle
+      ::Bundler.require
+    rescue ::Bundler::GemfileNotFound
     end
   end
 end

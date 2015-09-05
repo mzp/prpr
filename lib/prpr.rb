5f584c6f4c407759fcb73915dacabe38e2f8cb96
mzp <mzpppp@gmail.com>
2015-09-05 12:34:15 +0900

    :fire: logging plugin

--- lib/prpr.rb
+++ lib/prpr.rb
@@ -5,8 +5,4 @@ require 'prpr/action/base'
 require 'prpr/handler/base'
 require 'prpr/publisher/message'
 require 'prpr/publisher/adapter/base'
-
 require 'prpr/publisher/adapter/console'
-
-require 'prpr/handler/log'
-require 'prpr/action/log/publish'
--- lib/prpr/handler/log.rb
+++
@@ -1,9 +0,0 @@
-module Prpr
-  module Handler
-    class Log < Base
-      handle AnyEvent do
-        Prpr::Action::Log::Publish.new(event).call
-      end
-    end
-  end
-end
